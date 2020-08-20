1e199058c3a890f3029ab7cae6c630608c014953
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-19 22:24:55 -0300

    Adiconando header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text,Image,View } from 'react-native';
 
 import { Icon } from 'react-native-elements';
-import { Container,TitleBold } from './styles';
+import { 
+    Container,
+    Title,TitleBold,
+    ImageDiv
+ } from './styles';
 
+const logo = require('../../../assets/img/logo.png');
 
 export default function Header(){
 
     return (
         <Container>
-            <TitleBold>Embuarama</TitleBold>               
+
+            <ImageDiv>
+                <Image source={logo} height={200} width={150} />
+            </ImageDiv>
+
+            <View> 
+                <TitleBold>EMBUARAMA</TitleBold>               
+                <Title>CONSULTORIA</Title>
+            </View>
+            
         </Container>
     );
 }
--- src/components/layout/header/styles.tsx
+++ src/components/layout/header/styles.tsx
@@ -6,16 +6,27 @@ export const Container = styled.View`
     padding-bottom: 2%;
     flex-direction: row; 
     display: flex;
+    box-shadow: 10px 2px 2px #E3E3E3;
 `;
 
 export const TitleBold = styled.Text`
+    margin-top: 9%;
+    margin-left: 5%;
     font-family: OpenSans_700Bold;
-    font-size : 14px;
+    font-size : 18px;
     color: #1D293F;
 `;
 
 export const Title = styled.Text`
+    margin-top: 2%;
+    margin-left: 5%;
     font-family: OpenSans_400Regular;
-    font-size : 14px;
-    font-color: #1D293F;
+    font-size : 18px;
+    color: #1D293F;
+`;
+
+export const ImageDiv = styled.View`
+    padding-top: 2%;
+    padding-left: 15%;
+    margin-left: 5%;
 `;
