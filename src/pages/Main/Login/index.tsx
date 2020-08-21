a3a15233fa4994f988ff3ba7a1da8679b3c3ee72
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-20 23:25:17 -0300

    Configurando axios

--- src/pages/Main/Login/index.tsx
+++ src/pages/Main/Login/index.tsx
@@ -2,25 +2,41 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import { Container,View,TouchableOpacity,Text } from './styles';
-
-
 import Header from '../../../components/layout/header';
 import Input from '../../../components/input/text';
 import ButtonGrey from '../../../components/input/buttons/grey';
 import Checkbox from '../../../components/input/checkbox';
+
+import api from '../../../services/api';
 
 export default function Login(){
 
     const navigation = useNavigation();
 
     function handleNavigateToHome(){
-        navigation.navigate('Home');
+        console.log('Chegou')
+        api.request({   
+            method: 'GET',
+            url: `/user`,
+            params: {
+                ds_login: "ds_email",
+                ds_senha : "ds_senha"
+            },
+          
+          }).then(function(response){
+              console.log("Response")
+              console.log(response)
+          }).catch(function(err){
+              console.log(err)
+          });
+
+
     }
 
     return (
         <Container>
 
-            <Header />
+            <Header /> 
 
             <View>
 
