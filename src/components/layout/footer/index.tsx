e7b6ccb5545542975ca4cc23007160b586b2cb2b
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-22 17:57:04 -0300

    Configurando rotas

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, TouchableOpacity } from 'react-native';
 import { Container, Content, Footer, FooterTab, Button, Text } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 
-export default function FooterTabs(){
+interface Iprops{
+    page : string,
+}
+export default function FooterTabs(props : Iprops){
+
+    const { page } = props;
+
+    const navigation = useNavigation();
+
+    function goBack(){
+        navigation.navigate(page);
+    }
+
+    function goConfig(){
+
+    }
+
+    function goHome(){
+
+    }
+
     return (
       <Container>
         <Content />
         <Footer>
-          <FooterTab style={{backgroundColor: '#FFFFFF'}}>
+          <FooterTab style={{backgroundColor: '#FFFFFF', borderTopWidth:1}}>
+
             <Button>
-                <View style={{borderRadius: 50, borderColor: '#000', borderWidth: 1, paddingLeft: 5, paddingRight: 5}}>
-                    <Ionicons name="md-arrow-back" size={32} color="black" />
-                </View>
+                <TouchableOpacity onPress={goBack}>
+                    <View style={{borderRadius: 50, borderColor: '#000', borderWidth: 1, paddingLeft: 5, paddingRight: 5}}>
+                        <Ionicons name="md-arrow-back" size={32} color="black" />
+                    </View>
+                </TouchableOpacity>
             </Button>
+
             <Button>
-                <Ionicons name="md-home" size={32} color="black" />
+                <TouchableOpacity>
+                    <Ionicons name="md-home" size={32} color="black" />
+                </TouchableOpacity>
             </Button>
+
             <Button>
-                <Ionicons name="md-cog" size={32} color="black" />
+                 <TouchableOpacity>
+                    <Ionicons name="md-cog" size={32} color="black" />
+                </TouchableOpacity>
             </Button>
+
           </FooterTab>
         </Footer>
       </Container>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,12 +13,12 @@ export default function Home(){
             <Container>
                 <Header />
             </Container>
-            
+
             <SafeAreaView>    
                 <Container>
                 </Container>
             </SafeAreaView>
-            <Footer/>
+            <Footer page={'Login'}/>
         </>
     );
 }
